4b9365a5eb10ec8af4cc8e86105d5eab9f06a5f4
Vikash2026 <96017594+Vikash2026@users.noreply.github.com>
2021-12-12 12:30:58 -0500

    Update example.py

--- example.py
+++ example.py
@@ -2,43 +2,22 @@ from Kitsune import Kitsune
 import numpy as np
 import time
 
-##############################################################################
-# Kitsune a lightweight online network intrusion detection system based on an ensemble of autoencoders (kitNET).
-# For more information and citation, please see our NDSS'18 paper: Kitsune: An Ensemble of Autoencoders for Online Network Intrusion Detection
-
-# This script demonstrates Kitsune's ability to incrementally learn, and detect anomalies in recorded a pcap of the Mirai Malware.
-# The demo involves an m-by-n dataset with n=115 dimensions (features), and m=100,000 observations.
-# Each observation is a snapshot of the network's state in terms of incremental damped statistics (see the NDSS paper for more details)
-
-#The runtimes presented in the paper, are based on the C++ implimentation (roughly 100x faster than the python implimentation)
-###################  Last Tested with Anaconda 3.6.3   #######################
-
-# Load Mirai pcap (a recording of the Mirai botnet malware being activated)
-# The first 70,000 observations are clean...
-print("Unzipping Sample Capture...")
+print("Unzipping file...")
 import zipfile
-with zipfile.ZipFile("mirai.zip","r") as zip_ref:
+with zipfile.ZipFile("data.zip","r") as zip_ref:
     zip_ref.extractall()
-
-
 # File location
-path = "mirai.pcap" #the pcap, pcapng, or tsv file to process.
-packet_limit = np.Inf #the number of packets to process
-
-# KitNET params:
-maxAE = 10 #maximum size for any autoencoder in the ensemble layer
-FMgrace = 5000 #the number of instances taken to learn the feature mapping (the ensemble's architecture)
-ADgrace = 50000 #the number of instances used to train the anomaly detector (ensemble itself)
-
-# Build Kitsune
-K = Kitsune(path,packet_limit,maxAE,FMgrace,ADgrace)
+path = "data.pcap" 
+packet_limit = 
+maxAE = 10 
+TLMG = 100
+NLBG = 100
+K = Kitsune(path,packet_limit,maxAE,TLMG,NLBG)
 
 print("Running Kitsune:")
 RMSEs = []
 i = 0
 start = time.time()
-# Here we process (train/execute) each individual packet.
-# In this way, each observation is discarded after performing process() method.
 while True:
     i+=1
     if i % 1000 == 0:
@@ -50,18 +29,15 @@ while True:
 stop = time.time()
 print("Complete. Time elapsed: "+ str(stop - start))
 
-
-# Here we demonstrate how one can fit the RMSE scores to a log-normal distribution (useful for finding/setting a cutoff threshold \phi)
 from scipy.stats import norm
-benignSample = np.log(RMSEs[FMgrace+ADgrace+1:100000])
-logProbs = norm.logsf(np.log(RMSEs), np.mean(benignSample), np.std(benignSample))
+bs = np.log(RMSEs[TLMG+NLBG+1:100000])
+logP = norm.logsf(np.log(RMSEs), np.mean(bs), np.std(bs))
 
-# plot the RMSE anomaly scores
 print("Plotting results")
 from matplotlib import pyplot as plt
 from matplotlib import cm
 plt.figure(figsize=(10,5))
-fig = plt.scatter(range(FMgrace+ADgrace+1,len(RMSEs)),RMSEs[FMgrace+ADgrace+1:],s=0.1,c=logProbs[FMgrace+ADgrace+1:],cmap='RdYlGn')
+fig = plt.scatter(range(TLMG+NLBG+1,len(RMSEs)),RMSEs[TLMG+NLBG+1:],s=0.1,c=logProbs[TLMG+NLBG+1:],cmap='RdYlGn')
 plt.yscale("log")
 plt.title("Anomaly Scores from Kitsune's Execution Phase")
 plt.ylabel("RMSE (log scaled)")
